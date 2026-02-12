cd905efc917dcba5447f229a31d0e896edf6cbbe
Xavier Sanna <22643870+xavier-sanna@users.noreply.github.com>
2026-02-13 00:44:56 +0100

    init superbar

--- superbar/superbar.tsx
+++ superbar/superbar.tsx
@@ -1,16 +1,18 @@
 import app from "ags/gtk4/app";
+import scss from "./superbar.scss";
 import { Astal } from "ags/gtk4";
 import { createPoll } from "ags/time";
 
 app.start({
+  css: scss,
   main() {
     const { TOP, LEFT, RIGHT } = Astal.WindowAnchor;
     const clock = createPoll("", 1000, "date");
 
     return (
-      <window visible anchor={TOP | LEFT | RIGHT}>
+      <window visible anchor={TOP}>
         <label label={clock} />
-        <label label="chocapic" />
+        <label label="test" />
       </window>
     );
   },
